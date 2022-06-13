22163cfaf4c45eee448affc6815f5f069d81fae7
falahputra <putrafalah9@gmail.com>
2022-06-13 20:51:21 +0800

    Dah selesai

--- script.js
+++ script.js
@@ -26,9 +26,84 @@ const updateScreen = (number) => {
 }
 
 const inputNumber = (number) =>{
-    currentNumber += number;
+    if(currentNumber === "0"){
+        currentNumber = number;
+    }else{
+        currentNumber += number;
+    }
 }
 
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
 
+const inputOperator = (operator) =>{
+    if (calculationOperator === ""){
+        prevNumber = currentNumber;
+        
+    }
+    calculationOperator = operator;
+    currentNumber = "";
+}
 
+const equalSign = document.querySelector(".equal-sign");
+equalSign.addEventListener("click", () => {
+    console.log("equal buton is pressed")
+    calculate()
+    updateScreen(currentNumber)
+    
+})
 
+const calculate = () =>{
+    let result = "";
+    switch(calculationOperator){
+        case "+" :
+            result = parseFloat(prevNumber) + parseFloat(currentNumber);
+            break;
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber);
+            break;
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber);
+            break;
+        case  "/": 
+            result = parseFloat(prevNumber) / parseFloat(currentNumber);
+            break;
+        default:
+            break;
+    }
+    currentNumber = result;
+    calculationOperator = "";
+}
+
+const clearBtn = document.querySelector(".all-clear")
+clearBtn.addEventListener("click",() =>{
+    clearAll()
+    updateScreen(currentNumber)
+    console.log("AC button is pressed")
+})
+
+const clearAll = ()=>{
+    prevNumber = "";
+    calculationOperator = "";
+    currentNumber = "0";
+
+}
+
+const decimal = document.querySelector(".decimal")
+decimal.addEventListener("click", (evemt)=>{
+    inputDecimal(evemt.target.value)
+    updateScreen(currentNumber)
+    console.log(evemt.target.value)
+})
+
+inputDecimal = (dot) =>{
+    if (currentNumber.includes(".")){
+        return
+    }else{
+        currentNumber += dot
+    }
+}
